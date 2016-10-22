b638dc8610b9e5f239a33a177e846195d5526373
Debbie Yiu <debbie.yiu.0219@gmail.com>
2016-10-22 18:18:00 +0800

    CustomerData index, create, details, edit

---
+++ CustomerMgmt/Controllers/CustomerDataController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CustomerMgmt.Models;
+
+namespace CustomerMgmt.Controllers
+{
+    public class CustomerDataController : Controller
+    {
+		private CustomerEntities db = new CustomerEntities();
+
+		// GET: CustomerData
+		public ActionResult Index()
+        {
+			var data = db.客戶資料.OrderByDescending(customer => customer.Id);
+			return View(data);
+		}
+
+		public ActionResult Create()
+		{
+			var customer = new 客戶資料();
+			return View(customer);
+		}
+
+		[HttpPost]
+		public ActionResult Create(客戶資料 customer)
+		{
+
+			db.客戶資料.Add(customer);
+			try
+			{
+				db.SaveChanges();
+			}
+			catch (DbEntityValidationException exception)
+			{
+				foreach (var ex in exception.EntityValidationErrors)
+				{
+					foreach (var error in ex.ValidationErrors)
+					{
+						throw new DbEntityValidationException(string.Format("Exception on {0} : {1}", error.PropertyName, error.ErrorMessage));
+					}
+				}
+			}
+			return RedirectToAction("Index");
+
+		}
+
+		public ActionResult Edit(int Id)
+		{
+			var customer = db.客戶資料.FirstOrDefault(x => x.Id.Equals(Id));
+			return View(customer);
+		}
+
+		public ActionResult Details(int Id)
+		{
+			var customer = db.客戶資料.FirstOrDefault(x => x.Id.Equals(Id));
+			return View(customer);
+		}
+	}
+}
