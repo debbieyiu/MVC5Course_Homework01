caafc3b0dd846dc7b212aab43bad988608e82278
Debbie Yiu <debbie.yiu.0219@gmail.com>
2016-10-22 17:25:26 +0800

    remove excess views

--- CustomerMgmt/Controllers/HomeController.cs
+++ CustomerMgmt/Controllers/HomeController.cs
@@ -12,19 +12,5 @@ namespace CustomerMgmt.Controllers
 		{
 			return View();
 		}
-
-		public ActionResult About()
-		{
-			ViewBag.Message = "Your application description page.";
-
-			return View();
-		}
-
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
-
-			return View();
-		}
 	}
 }
